2ae705d13ab7f283e4e174a0bf9a40c37d83f3b1
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 00:08:49 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 
 def meal_choice(first_kind_veg, second_kind_veg, protein ="meat")
 puts "What a nutritious meal!"
-p "A plate of #{protein} with #{first_kind_veg} and #{second_kind_veg}."
+puts "A plate of #{protein} with #{first_kind_veg} and #{second_kind_veg}."
 end

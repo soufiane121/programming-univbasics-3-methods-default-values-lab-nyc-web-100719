bb126782630d0996b3d048847b061894dbbaf6a2
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 00:11:05 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,7 @@
 # Your code here
+
+def meal_choice(first_kind_veg, second_kind_veg, protein ="meat")
+puts "What a nutritious meal!"
+puts last_message= "A plate of #{protein} with #{first_kind_veg} and #{second_kind_veg}."
+ last_message
+end

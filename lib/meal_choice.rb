8c4b4e3b4ef037dacac44a0b48545b7ba1c40ff5
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 00:07:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 # Your code here
 
 def meal_choice(first_kind_veg, second_kind_veg, protein ="meat")
-print "What a nutritious meal"
+puts "What a nutritious meal"
 p "A plate of #{protein} with #{first_kind_veg} and #{second_kind_veg}."
 end

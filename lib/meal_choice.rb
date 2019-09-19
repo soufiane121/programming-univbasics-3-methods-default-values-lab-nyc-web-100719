9c89330e80621ba4842f4618ecac79ac33770fa6
soufiane oucherrou <soufiane.oucherrou@gmail.com>
2019-09-19 00:10:51 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,5 @@
 def meal_choice(first_kind_veg, second_kind_veg, protein ="meat")
 puts "What a nutritious meal!"
 puts last_message= "A plate of #{protein} with #{first_kind_veg} and #{second_kind_veg}."
-return last_message
+ last_message
 end
